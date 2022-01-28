ae1be685965fcf223c64488725c178c887afc52c
Muhammed Soysal <soysalmuhammed000@gmail.com>
2022-01-29 00:31:06 +0300

    C# Kamp

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class MyDictionary<TKey, TValue>
+    {
+        TKey[] tkey;
+        TValue[] tvalue;
+        public MyDictionary()
+        {
+            tkey = new TKey[0];
+            tvalue = new TValue[0];
+        }
+        public void Add(TKey tkeyy, TValue tvaluee)
+        {
+            TKey[] tempKeyArray = tkey;
+            tkey = new TKey[tkey.Length + 1];
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                tkey[i] = tempKeyArray[i];
+            }
+            tkey[tkey.Length - 1] = tkeyy;
+
+            TValue[] tempValueArray = tvalue;
+            tvalue = new TValue[tempValueArray.Length + 1];
+            for (int i = 0; i < tempValueArray.Length; i++)
+            {
+                tvalue[i] = tempValueArray[i];
+            }
+            tvalue[tvalue.Length - 1] = tvaluee;
+        }
+
+        public TValue[] Valuee
+        {
+            get { return tvalue; }
+        }
+        public TKey[] Keyy
+        {
+            get { return tkey; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Aynen derste yaptığımız MyList gibi siz de kendi Dictonary sınıfınızı yazınız. (MyDictionary)
+             Sadece ekleme (Add) metodunu yazınız.
+             */
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            dic.Add(1, "Ahmet");
+            dic.Add(2, "Muhammed");
+            foreach (var dickisi in dic)
+            {
+                Console.WriteLine(dickisi);
+            }
+            Console.WriteLine(dic.Count);   
+
+            Console.WriteLine("------------------------");
+
+            MyDictionary  <int,string> myDictionary = new MyDictionary<int,string>();
+            myDictionary.Add(1, "Muhammed");
+            myDictionary.Add(2, "Sıla");
+            myDictionary.Add(3, "Elif");
+            myDictionary.Add(4, "Muhammet Ali");
+            foreach (var kisi in myDictionary.Valuee)
+            {
+                Console.WriteLine(kisi);
+            }
+            foreach (var kisi in myDictionary.Keyy)
+            {
+                Console.WriteLine(kisi);
+            }
+        }
+    }
+}
