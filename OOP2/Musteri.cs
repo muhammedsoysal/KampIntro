3df990e6532e7801939651c0e14071b27bdad787
Muhammed Soysal <soysalmuhammed000@gmail.com>
2022-01-29 23:53:58 +0300

    C# Kamp

---
+++ OOP2/Musteri.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOP2
+{
+    internal class Musteri
+    {
+        public int Id { get; set; }
+        public string MusteriNo { get; set; }
+    }
+}
---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    //İş sınıfları -- Business Class
+    internal class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+        }
+    }
+}
